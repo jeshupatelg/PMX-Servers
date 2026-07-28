624141384501d517b776f27a54c342a5c2702788
Jeshu Patel <jeshupatelg3774@gmail.com>
2026-07-28 17:27:25 +0530

    adding external networks for pg, kafka and redis

--- network/network-compose.sh
+++ network/network-compose.sh
@@ -3,3 +3,6 @@
 # Create the network only if it doesn't already exist
 docker network inspect gateway_net >/dev/null 2>&1 || docker network create gateway_net
 docker network inspect minikube_net >/dev/null 2>&1 || docker network create minikube_net
+docker network inspect kafka_net >/dev/null 2>&1 || docker network create kafka_net
+docker network inspect postgres_net >/dev/null 2>&1 || docker network create postgres_net
+docker network inspect redis_net >/dev/null 2>&1 || docker network create redis_net
